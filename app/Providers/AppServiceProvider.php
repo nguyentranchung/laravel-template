8415ee9b89bd16c387be5703afd87a51ab5f7261
Nguyen Tran Chung <nguyentranchung52th@gmail.com>
2022-04-08 08:16:52 +0900

    preventLazyLoading

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Model::preventLazyLoading(! App::isProduction());
     }
 }
